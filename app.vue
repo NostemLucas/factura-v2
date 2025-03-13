bec6298497108cb6f1c31d763ba2bcda6c185821
NostemLucas <102114238+NostemLucas@users.noreply.github.com>
2025-03-12 22:34:04 -0400

    fix: correcion de errores de eslint

--- app.vue
+++ app.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
     <NuxtPage />
-    <Toaster richColors />
   </div>
 </template>
 
 <script setup lang="ts">
-import { Toaster } from "vue-sonner";
+import { Toaster } from 'vue-sonner'
 </script>
