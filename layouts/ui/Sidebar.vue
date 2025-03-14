26fada9f13659b846f84f2b075e7bba65e05c574
Alexander Nina <aaalex2025@gmail.com>
2025-03-14 10:10:35 -0400

    fix: reponsive sidebar fixed

--- layouts/ui/Sidebar.vue
+++ layouts/ui/Sidebar.vue
@@ -16,12 +16,12 @@ const { toggleDrawer } = dashboard
 <template>
   <div
     v-show="openDrawer"
-    class="fixed inset-0 z-0 bg-black/20 lg:hidden transition-all duration-300"
+    class="fixed inset-0 z-20 bg-black/20 lg:hidden transition-all duration-300"
     @click="toggleDrawer"
   ></div>
   <aside
     :class="[
-      'fixed inset-y-0 left-0 z-0 h-screen flex w-32 flex-col border-r border-gray-200 bg-white transition-all pt-16 duration-300  overflow-x-hidden',
+      'fixed inset-y-0 left-0 z-50 lg:z-0 h-screen flex w-64 lg:w-16 flex-col border-r border-gray-200 bg-white transition-all lg:pt-16 duration-300  overflow-x-hidden',
       lgUp && (openDrawer ? 'lg:w-48' : 'lg:w-16'),
       !lgUp && (openDrawer ? 'translate-x-0' : '-translate-x-full')
     ]"
@@ -39,7 +39,7 @@ const { toggleDrawer } = dashboard
                 'flex w-full flex-col items-center justify-center rounded-lg gap-2 p-2 text-[10px] text-gray-600 transition-colors hover:bg-gray-100',
                 item.url == router.fullPath ? 'bg-gray-100 text-blue-600' : '',
                 openDrawer
-                  ? 'lg:flex-row lg:justify-start lg:gap-3 lg:px-3 lg:text-xs'
+                  ? 'flex-row lg:flex-row lg:justify-start lg:gap-3 lg:px-3 lg:text-xs'
                   : ''
               ]"
             >
@@ -58,7 +58,7 @@ const { toggleDrawer } = dashboard
     <div class="mt-auto border-t border-gray-200 p-2">
       <div
         class="flex cursor-pointer flex-col gap-2 items-center rounded-lg p-2 text-[10px] text-gray-600 hover:bg-gray-100"
-        :class="openDrawer ? 'lg:flex-row lg:gap-3 lg:text-xs' : ''"
+        :class="openDrawer ? 'flex-row lg:flex-row lg:gap-3 lg:text-xs' : ''"
       >
         <user-icon class="mb-1 h-5 w-5 lg:mb-0" />
         <span class="text-center">Perfil</span>
